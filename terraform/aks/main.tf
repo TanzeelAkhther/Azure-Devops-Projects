9143467933df7052de69efe5a60022cea8f27d7d
Tanzeel <tanzeelakther949@gmail.com>
2026-01-06 01:15:22 +0530

    Added AKS infra and subnet modifications

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -5,3 +5,45 @@ resource "azurerm_resource_group" "aks_rg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "vnet-aks-dev"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  address_space       = ["10.0.0.0/16"]
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "subnet-aks-dev"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "dev_cluster" {
+  name                = "aks-dev-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aksdevcluster"
+  kubernetes_version  = "1.33.5"
+  default_node_pool {
+    name           = "defaultaks"
+    node_count     = 2
+    vm_size        = "Standard_D2s_v3"
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = "10.1.0.0/16"
+    dns_service_ip = "10.1.0.10"
+  }
+  tags = {
+    environment = "dev"
+  }
+}
